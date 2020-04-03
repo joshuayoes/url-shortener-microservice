f2894dc43df37b3ef4a55e56b4cc482d9ad33c13
Joshua Yoes <>
2020-04-03 11:08:22 -0700

    Add freeCodeCamp boilerplate

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,25 @@
-import express, {Application} from 'express';
+import express, {Application, Request, Response} from 'express';
+import mongo from 'mongodb';
+import mongoose from 'mongoose';
+import cors from 'cors';
 
+// Basic Configuration 
 const app: Application = express();
+var port = process.env.PORT || 3000;
 
-app.listen(3000, () => console.log('Listening on port 3000...'))
+/** this project needs a db !! **/ 
+// mongoose.connect(process.env.DB_URI);
+
+app.use(cors());
+
+/** this project needs to parse POST bodies **/
+// you should mount the body-parser here
+
+app.use('/public', express.static(__dirname + '/public'));
+
+app.get('/', (req: Request, res: Response) => {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+
+app.listen(port, () => console.log('Listening on port 3000...'))
